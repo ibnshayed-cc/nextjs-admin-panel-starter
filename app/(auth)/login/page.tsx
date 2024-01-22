493e9566292ff548ff490583c34db478a43c9290
mdemranbd <md.emran@nusaiba.com.bd>
2024-01-22 15:44:06 +0600

    [DONE] login page implemented

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,9 @@
+import { LoginFormPage } from "../../ui/form/LoginForm";
+
 function LoginPage() {
-  return <div className="flex justify-center items-center"></div>;
+  return <div className="h-screen flex justify-center items-center">
+    <LoginFormPage/>
+  </div>;
 }
 
 export default LoginPage;
